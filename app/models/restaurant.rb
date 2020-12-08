22265799e28aab2a4b94e7a588775ce1d31dcde3
Pascal Fodiman <pascal@fodiman.fr>
2020-12-08 18:09:12 +0100

    validations of Review and Restaurant models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,7 @@
 class Restaurant < ApplicationRecord
-  has_many :reviews
-  validates :name, :address, :category, presence: true
+  has_many :reviews, dependent: :destroy
+  validates :name, :address, presence: true
+  validates :category, :address, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
+    message: "%{value} is not a valid category" }
 end
